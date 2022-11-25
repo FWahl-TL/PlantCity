b36adbdac6e0383a22667f1ace9eff3bab14b065
Florian Wahl <florianwahl2@gmail.com>
2022-11-25 20:12:34 +0100

    git migration

---
+++ PlantCity/Core/Notify.cs
@@ -0,0 +1,63 @@
+﻿using Notification.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantCity.Core
+{
+    public static class Notify
+    {
+        private static bool endLogging = false;
+        static NotificationManager notificationManager = new NotificationManager();
+        public static void sendInfo(string message)
+        {
+            if (endLogging) return;
+            notificationManager.Show(new NotificationContent
+            {
+                Title = "Info",
+                Message = message,
+                Type = NotificationType.Information
+            });
+        }
+        public static void sendWarn(string message)
+        {
+            if (endLogging) return;
+            notificationManager.Show(new NotificationContent
+            {
+                Title = "Warning!",
+                Message = message,
+                Type = NotificationType.Warning
+            });
+        }
+        public static void sendSuccess(string message)
+        {
+            if (endLogging) return;
+            notificationManager.Show(new NotificationContent
+            {
+                Title = "Success!",
+                Message = message,
+                Type = NotificationType.Success
+            });
+        }
+        public static void sendError(string message)
+        {
+            if (endLogging) return;
+            notificationManager.Show(new NotificationContent
+            {
+                Title = "Error!",
+                Message = message,
+                Type = NotificationType.Error
+            });
+        }
+        public static void EndLogging()
+        {
+            endLogging = true;
+        }
+        public static void ResumeLogging()
+        {
+            endLogging = false;
+        }
+    }
+}
---
+++ PlantCity/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using PlantCity.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlantCity.MVVM.ViewModel;
+
+namespace PlantCity.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
+        public RelayCommand AboutViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }
+        public AboutViewModel AboutVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            SettingsVM = new SettingsViewModel();
+            AboutVM = new AboutViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => {CurrentView = HomeVM;});
+            SettingsViewCommand = new RelayCommand(o => { CurrentView = SettingsVM; });
+            AboutViewCommand = new RelayCommand(o => { CurrentView = AboutVM; });
+        }
+
+    }
+}
