1ca0fd11fefc93da5c361a2d7faff0587c8ed87b
Florian Wahl <FWahl@TimeLine.de>
2022-12-02 12:31:06 +0100

    Added Connection Tab and Com Ports

---
+++ PlantCity/MVVM/View/ConnectorView.xaml.cs
@@ -0,0 +1,87 @@
+﻿using PlantCity.Core;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PlantCity.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for ConnectorView.xaml
+    /// </summary>
+    public partial class ConnectorView : UserControl
+    {
+        public List<string> ComPorts { get; set; }
+
+        public SerialPort SerialConnection {get;set;}
+
+        public ConnectorView()
+        {
+            InitializeComponent();
+            Loaded += ConnectorView_Loaded;
+        }
+
+        private void ConnectorView_Loaded(object sender, RoutedEventArgs e)
+        {
+            CheckPorts();
+            SetErrorStatus("Stopped");
+        }
+
+        public void CheckPorts()
+        {
+            ComPorts = SerialPort.GetPortNames().ToList();
+            CBPort.ItemsSource = ComPorts;
+        }
+
+        public void StartConnection()
+        {
+            if(string.IsNullOrEmpty(CBPort?.SelectedValue?.ToString())) {
+                Notify.sendError("No Port Selected");
+                SetErrorStatus("Stopped");
+                return;
+            }
+            SerialConnection = new SerialPort(CBPort.SelectedValue.ToString(), 155200);
+            SetStatus("Opening Serial Connection...");
+            SerialConnection.Open();
+            SetStatus("Port ready");
+
+            //DEMO:
+            SerialConnection.Write("Hello World");
+            var x = SerialConnection.Read(new byte[] { }, 0, 255);
+        }
+            
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Notify.sendSuccess("Stared Connection");
+            StartConnection();
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Notify.sendSuccess("Stopped");
+            SetErrorStatus("Stopped");
+        }
+
+        public void SetStatus(string msg)
+        {
+            StatusText.Foreground = Brushes.White;
+            StatusText.Content = msg;
+        }
+        public void SetErrorStatus(string msg)
+        {
+            StatusText.Foreground = Brushes.Red;
+            StatusText.Content = msg;
+        }
+    }
+}
