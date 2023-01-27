f665d1dc58585c25d541a49f571e735461bb0601
tljbonne <jbonne@timeline.de>
2023-01-27 14:22:34 +0100

    Update slave.ino

--- Script/slave/slave.ino
+++ Script/slave/slave.ino
@@ -2,9 +2,16 @@
 #include "ArduinoJson.h"
 #include "ESP8266HTTPClient.h"
 
+//Wlan
 const char* ssid = "OnePlus"; // Enter your WiFi SSID
 const char* password = "1234asd1234"; // Enter Password
-#define led 4
+
+//Weather Api
+const char* host = "api.openweathermap.org";
+const char* apiKey = "your_API_KEY";
+const char* city = "Wuppertal";
+const char* units = "metric";
+bool weatherRecieved = true;
 
 double measured_humidity = 0;
 double measured_tank = 0;
@@ -13,7 +20,6 @@ double current_temp = 0;
 double current_rain = 0;
 
 void setup() {
-pinMode(led, OUTPUT);
 Serial.begin(9600);
 // Connect to WiFi
 WiFi.begin(ssid, password);
@@ -32,13 +38,11 @@ Serial.print(WiFi.localIP());
 
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
-    digitalWrite(led, HIGH);
     //Read the sensor values
     measured_humidity = readHumidity();
     measured_tank = readTank();
     measured_light = readLight();
-    current_temp = readTemp();
-    current_rain = readRain();
+    getCurrentWeatherData();
     //Create a JSON object
     StaticJsonDocument<200> jsonDoc;
     jsonDoc["measured_humidity"] = measured_humidity;
@@ -74,28 +78,37 @@ void sendData(StaticJsonDocument<200> jsonDoc){
   http.end();
 }
 
-float readHumidity(){
+double readHumidity(){
 //Replace with code to read humidity sensor
 return 0;
 }
 
-float readTank(){
+double readTank(){
 //Replace with code to read tank sensor
 return 0;
 }
 
-float readLight(){
+double readLight(){
 //Replace with code to read light sensor
 return 0;
 }
 
-float readTemp(){
-//Replace with code to read temperature sensor
-return 0;
+getCurrentWeatherData(){
+  weatherRecieved = true;
+  char* url = String("/data/2.5/weather?q=") + city + "&units=" + units + "&appid=" + apiKey;
+
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    String payload = http.getString();
+    StaticJsonDocument<256> doc;
+    deserializeJson(doc, payload);
+    current_temp = doc["main"]["temp"];
+    current_rain = doc["rain"]["h1"];
+    Serial.println("request success Temp: " + current_temp + " rain: " + current_rain);
+  } else {
+    Serial.println("Error on HTTP request: " + String(httpCode));
+  }
+  http.end();
+  delay(60000);
 }
 
-float readRain(){
-  //Replace with code to read rain sensor
-return 0;
-}
-}
